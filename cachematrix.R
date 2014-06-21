acef3921b095a05173659b1c248f34f303bb1f5e
Ivan Ivanov <vankataivanov@gmail.com>
2014-06-21 03:09:14 +0300

    Implemented assigment functions for creating cache matrix data structure and calculating the inverse of it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions that given a matrix prepare a data structure 
+## which stores the matrix itself as well as cached computations 
+## and operate over this data structure.
 
-## Write a short comment describing this function
-
+## Given a matrix x returns a list with functions
+## for getting/setting the matrix and caching its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  m3xi <- NULL
+  set <- function(m) {
+    x <<- m
+    m3xi <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(i) m3xi <<- i
+  getinverse <- function() m3xi
+  
+  list(set = set, get = get, 
+       setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## calculates the inverse of the given matrix 
+## returned from makeCacheMatrix(...).
+## If a cached value is found calculations are skipped
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if ( is.null(x$getinverse()) ) { # if(no cache) calc and cache
+    x$setinverse( solve(x$get()) )
+  }
+  return(x$getinverse())
 }
